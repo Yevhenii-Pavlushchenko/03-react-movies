b0207af9cdbace34acb6d9305da2851824eb958a
JustFun <paw93@ukr.net>
2026-03-11 18:26:12 +0200

    add SearchBar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,24 @@
 // import { useState } from 'react'
 import './App.module.css'
 
+import SearchBar from '../SearchBar/SearchBar'
+import  { toast, Toaster }from 'react-hot-toast'
+import fetchMovies from '../../services/movieService'
+
 function App() {
 
+  const handleSearch = async (query: string) => {
+    
+    const data = await fetchMovies(query)
+    if (data.length === 0 ) {
+      toast("No movies found for your request.")
+    }
+  }
 
   return (
     <>
-      
+      <Toaster position="top-center" />
+      <SearchBar onSubmit={handleSearch} />
     </>
   )
 }
